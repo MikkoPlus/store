1bc18908ce224fc4ba8434a3b643f6ee72c611a8
Anatoliy Sharov <sharov.012@gmail.com>
2023-12-02 15:51:22 +0300

    feat(comp): change folder app component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Cart } from '../../pages/Cart/Cart';
+import Header from '../Header/Header';
+import { Catalog } from '../../pages/Catalog/Catalog';
+import { useCartActions, useProductActions } from '../../hooks/useActions';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import './App.css';
+import { NotFoundPage } from '../../pages/NotFoundPage/NotFoundPage';
+
+export const App: React.FC = () => {
+  const { fetchCartProducts } = useCartActions();
+  const { fetchProducts } = useProductActions();
+  const { cartProductsList } = useTypedSelector((state) => state.cartProducts);
+
+  useEffect(() => {
+    fetchProducts();
+    const localStorageData = localStorage.getItem('cart');
+    if (localStorageData) {
+      fetchCartProducts(JSON.parse(localStorageData));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cartProductsList));
+  }, [cartProductsList]);
+
+  return (
+    <div className='app'>
+      <Header />
+      <main className='app__content'>
+        <Routes>
+          <Route path='/' element={<Catalog />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/*' element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
